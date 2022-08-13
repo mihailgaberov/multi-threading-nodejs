1d63b7313c8350526bf81a4485d007acf8aef7c0
Mihail Gaberov <mihail.gaberov@gmail.com>
2022-08-13 10:01:39 +0200

    :student: Offloading a CPU-Bound Task Using Promises

--- index.js
+++ index.js
@@ -7,11 +7,18 @@ app.get("/non-blocking/", (req, res) => {
   res.status(200).send("This page is non-blocking");
 });
 
+function calculateCount() {
+  return new Promise((resolve, reject) => {
+    let counter = 0;
+    for (let i = 0; i < 20_000_000_000; i++) {
+      counter++;
+    }
+    resolve(counter);
+  });
+}
+
 app.get("/blocking", async (req, res) => {
-  let counter = 0;
-  for (let i = 0; i < 20_000_000_000; i++) {
-    counter++;
-  }
+  const counter = await calculateCount();
   res.status(200).send(`result is ${counter}`);
 });
 
