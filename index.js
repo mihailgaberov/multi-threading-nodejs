d952a8c78bdefaee98e9a3b6133fc7c747929113
Mihail Gaberov <mihail.gaberov@gmail.com>
2022-08-13 09:59:04 +0200

    :construction: Creating a CPU-Bound Task Without Worker Threads

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ app.get("/non-blocking/", (req, res) => {
   res.status(200).send("This page is non-blocking");
 });
 
+app.get("/blocking", async (req, res) => {
+  let counter = 0;
+  for (let i = 0; i < 20_000_000_000; i++) {
+    counter++;
+  }
+  res.status(200).send(`result is ${counter}`);
+});
+
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
